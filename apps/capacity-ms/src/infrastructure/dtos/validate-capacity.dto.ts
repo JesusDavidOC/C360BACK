5493ee642a75b88411709df0508c0adb0fbae44a
JesusDavidOC <jesusotero-98@hotmail.com>
2025-06-27 15:30:33 -0500

    last update

--- apps/capacity-ms/src/infrastructure/dtos/validate-capacity.dto.ts
+++ apps/capacity-ms/src/infrastructure/dtos/validate-capacity.dto.ts
@@ -1,7 +1,8 @@
 import { ServiceType } from '@c360/shared-kernel/enums/service-type.enum';
+import { Type } from 'class-transformer';
 import { IsDate, IsEnum } from 'class-validator';
-
 export class ValidateCapacityDto {
+  @Type(() => Date)
   @IsDate()
   date: Date;
 
